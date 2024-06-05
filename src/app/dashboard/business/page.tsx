783b8bf72f7a8dd7747ef0aae7c5ae128a2bbd4e
CharithCD <damruwanweerasinghe1@gmail.com>
2024-06-05 20:45:49 +0530

    template form for business page

--- src/app/dashboard/business/page.tsx
+++ src/app/dashboard/business/page.tsx
@@ -1,8 +1,94 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input';
+import { Loader2 } from 'lucide-react';
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const page = () => {
+  // const form = useForm<z.infer<typeof signupSchema>>({
+  //   resolver: zodResolver(signupSchema),
+  //   defaultValues: {
+  //     name: "",
+  //     email: "",
+  //     password: "",
+  //   },
+  // });
+
+  const form = useForm();
+
+  async function onSubmit() {
+
+  }
   return (
-    <div>page</div>
+    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Business</h1>
+      </div>
+      <div
+        className="flex flex-1 rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
+      >
+        <div className='p-8 w-full'>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Query Mate" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your company name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="sample@email.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="Password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
+                {form.formState.isSubmitting ? (
+                  <>
+                    <Loader2 className="animate-spin mr-2" /> Please wait...
+                  </>
+                ) : (
+                  "Signup"
+                )}
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </main>
   )
 }
 
